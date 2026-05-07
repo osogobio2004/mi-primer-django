a4aa46faccced99a1abb39823f91feb95712ef2f
osogobio2004 <rominaosogobio2004@gmail.com>
2026-05-06 22:42:00 -0600

    estilo!!

--- mi_proyecto/settings.py
+++ mi_proyecto/settings.py
@@ -130,3 +130,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- mi_proyecto/urls.py
+++ mi_proyecto/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings             # <-- NUEVO: Importamos las configuraciones
+from django.conf.urls.static import static   # <-- NUEVO: Importamos la función static
 
 urlpatterns = [
-    path ('', include('home.urls')),
-    path ('mi_aplicacion/', include('mi_aplicacion.urls')),
+    path('', include('home.urls')),
+    path('mi_aplicacion/', include('mi_aplicacion.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
